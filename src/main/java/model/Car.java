cd3556a7e6c159747a021c8f4cbf6c706263e583
Anıl T <aniltaskiran@stu.aydin.edu.tr>
2018-09-06 02:56:36 +0300

    car properties status

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -7,6 +7,15 @@ public class Car {
     private int brandId;
     private String zone;
     private String carOwnerPhone;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
     public String getZone() {
         return zone;
