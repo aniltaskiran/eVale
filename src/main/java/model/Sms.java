f5b3280ae930b3984e6af06c875e4ed1dbb0ba61
Canberk Sinangil <canberksinangil@gmail.com>
2018-09-14 21:27:36 +0300

    SMS MODEL ADDED

---
+++ src/main/java/model/Sms.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Sms {
+    private String username = "5422262375";
+    private String password = "MySmsService.evale!";
+    private String title = "ATLASMESAJ";
+    private String number;
+    private String message;
+    private String process = "1";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getProcess() {
+        return process;
+    }
+
+    public void setProcess(String process) {
+        this.process = process;
+    }
+}
