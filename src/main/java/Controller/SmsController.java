e3f0b72a1be21a966573f46f37846f7682c48a98
Canberk Sinangil <canberksinangil@gmail.com>
2018-09-15 15:40:46 +0300

    SNIPSHOT BEFORE TESTING SOMETHING

--- src/main/java/Controller/SmsController.java
+++ src/main/java/Controller/SmsController.java
@@ -1,6 +1,12 @@
 package Controller;
 
 import model.Sms;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 public class SmsController {
 
@@ -9,6 +15,16 @@ public class SmsController {
     }
 
     public void smsSend(Sms sms){
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(sms.getApiUrl());
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addTextBody(smsProperties.islem.toString(),sms.getProcess(), ContentType.TEXT_PLAIN);
+        builder.addTextBody(smsProperties.user.toString(),sms.getUsername(), ContentType.TEXT_PLAIN);
+        builder.addTextBody(smsProperties.pass.toString(),sms.getPassword(), ContentType.TEXT_PLAIN);
+
+        httpPost.setEntity(builder);
+
 
         //ADD MAVEN DEPENDENCIES for HTTPCLIENT
 
